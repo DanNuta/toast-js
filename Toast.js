4c36ef88bf235a0f636a6d8cdd4f8b707be9f9f9
dan.nuta <dan.nuta@ebs-integrator.com>
2024-08-06 09:00:47 +0300

    feat: add auto close to the toast

--- Toast.js
+++ Toast.js
@@ -1,16 +1,33 @@
+const DEFAULT_OPTIONSC = {
+  autoClose: 5000,
+  position: "top-left",
+};
+
 export default class Toast {
   #toastElement;
-  #positionToast;
-  constructor(options) {
-    this.#positionToast = options.position;
-    this.#toastElement = document.createElement("div");
-    Object.entries(options).forEach(([key, value]) => {
-      this[key] = value;
-    });
+  #autoCloseTimout;
 
-    this.getName;
+  /**
+   *
+   * @param {{
+   *  text: string,
+   *  position: "top-right" | "top-left" | "bottom_right" | "bottom-left"
+   *  autoClose?: boolean | number
+   * }} options
+   */
+  constructor(options) {
+    this.#toastElement = document.createElement("div");
+    Object.entries({ ...DEFAULT_OPTIONSC, ...options }).forEach(
+      ([key, value]) => {
+        this[key] = value;
+      }
+    );
   }
 
+  /**
+   *
+   * @param {"top-right" | "top-left" | "bottom_right" | "bottom-left"} value
+   */
   set position(value) {
     this.#toastElement.classList.add("toast");
 
@@ -21,6 +38,10 @@ export default class Toast {
     toastContainer.append(this.#toastElement);
   }
 
+  /**
+   *
+   * @param {string} text
+   */
   set text(text) {
     this.#toastElement.textContent = text;
   }
@@ -32,6 +53,23 @@ export default class Toast {
 
     if (!parentToastContainer.hasChildNodes()) {
       parentToastContainer.remove();
+    }
+  }
+
+  /**
+   *
+   * @param {boolean} value
+   */
+
+  set autoClose(value) {
+    if (this.#autoCloseTimout !== null) {
+      clearInterval(this.#autoCloseTimout);
+    }
+
+    if (value) {
+      this.#autoCloseTimout = setTimeout(() => {
+        this.remove();
+      }, value);
     }
   }
 }
--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 import Toast from "./Toast.js";
 
-const toast = new Toast({ position: "top-right", text: "New toast" });
-
-setTimeout(() => {
-  toast.remove();
-}, 1000);
+new Toast({
+  text: "New toast",
+});
